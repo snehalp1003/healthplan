3c01c46c88b93382841120de794dc4b476cf641c
snehalp1003 <patel.sneha@husky.neu.edu>
2021-03-20 14:53:06 +0530

    Auth changes

--- src/main/java/com/me/healthplan/service/HealthPlanAuthorizationService.java
+++ src/main/java/com/me/healthplan/service/HealthPlanAuthorizationService.java
@@ -26,40 +26,38 @@ import com.nimbusds.jwt.SignedJWT;
 
 @Service
 public class HealthPlanAuthorizationService {
-
+    
     static RSAKey rsaPublicJWK = null;
     static RSAKey rsaJWK = null;
     static {
-        try {
+         try {
 
-            // RSA signatures require a public and private RSA key pair, the
-            // public key
-            // must be made known to the JWS recipient in order to verify the
-            // signatures
-            rsaJWK = new RSAKeyGenerator(2048).keyID("123").generate();
-            rsaPublicJWK = rsaJWK.toPublicJWK();
+             // RSA signatures require a public and private RSA key pair, the public key
+             // must be made known to the JWS recipient in order to verify the signatures
+             rsaJWK = new RSAKeyGenerator(2048)
+                     .keyID("123")
+                     .generate();
+             rsaPublicJWK = rsaJWK.toPublicJWK();
 
-        } catch (JOSEException e) {
-            e.printStackTrace();
-        }
+         } catch (JOSEException e) {
+             e.printStackTrace();
+         }
 
-    }
-
+     }
+    
     public String generateToken() throws JOSEException {
+
 
         // Create RSA-signer with the private key
         JWSSigner signer = new RSASSASigner(rsaJWK);
 
         // Prepare JWT with claims set
-        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-                .issueTime(new Date(System.currentTimeMillis()))
-                .expirationTime(
-                        new Date(System.currentTimeMillis() + 1000 * 60 * 10))
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder().issueTime(new Date(System.currentTimeMillis()))
+                .expirationTime(new Date(System.currentTimeMillis() + 1000 * 60 * 10))
                 .build();
 
         SignedJWT signedJWT = new SignedJWT(
-                new JWSHeader.Builder(JWSAlgorithm.RS256)
-                        .keyID(rsaJWK.getKeyID()).build(),
+                new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(rsaJWK.getKeyID()).build(),
                 claimsSet);
 
         // Compute the RSA signature
@@ -67,7 +65,7 @@ public class HealthPlanAuthorizationService {
 
         return signedJWT.serialize();
     }
-
+    
     public String authorize(String authorization) {
 
         if (!authorization.contains("Bearer ")) {
@@ -76,26 +74,26 @@ public class HealthPlanAuthorizationService {
 
         String token = authorization.split(" ")[1];
 
-        try {
+            try {
 
-            SignedJWT signedJWT = SignedJWT.parse(token);
-            JWSVerifier verifier = new RSASSAVerifier(this.rsaPublicJWK);
+                SignedJWT signedJWT = SignedJWT.parse(token);
+                JWSVerifier verifier = new RSASSAVerifier(this.rsaPublicJWK);
 
-            // token is not valid
-            if (!signedJWT.verify(verifier)) {
-                return "Invalid Token";
+                // token is not valid
+                if(!signedJWT.verify(verifier)){
+                    return "Invalid Token";
+                }
+
+                Date expirationTime = signedJWT.getJWTClaimsSet().getExpirationTime();
+
+                // token ttl has expired
+                if(new Date().after(expirationTime)) {
+                    return "Token has expired";
+                }
+            } catch (JOSEException | ParseException e ){
+                System.out.println(e.getMessage());
+                return "Valid Token";
             }
-
-            Date expirationTime = signedJWT.getJWTClaimsSet()
-                    .getExpirationTime();
-
-            // token ttl has expired
-            if (new Date().after(expirationTime)) {
-                return "Token has expired";
-            }
-        } catch (JOSEException | ParseException e) {
-            return "Invalid Token";
-        }
 
         return "Valid Token";
     }
